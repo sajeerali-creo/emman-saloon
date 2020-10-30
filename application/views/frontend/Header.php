8dbb15d05737435fcf7a75145ffaad2341d73b3d
sajeer <sajeer.ali09@gmail.com>
2020-10-30 04:19:22 +0400

    done

--- application/views/frontend/Header.php
+++ application/views/frontend/Header.php
@@ -7,7 +7,8 @@
 		<head>
 		    <meta charset="utf-8">
 		    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link rel="icon" href="<?php echo base_url() ?>assets/frontend/img/fav-icon.png" type="image/gif" sizes="16x16">
 		    <meta name="description" content="">
 		    <meta name="author" content="">
 		    <title><?php echo $title; ?></title>
