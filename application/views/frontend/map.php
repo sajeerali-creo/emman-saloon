c63140eaf1a81fa6e2a302a4e9f395f1059b1ac8
Sarath C P <sarath.cp@vandalayglobal.com>
2020-11-01 02:06:31 +0530

    Update map.php

--- application/views/frontend/map.php
+++ application/views/frontend/map.php
@@ -1,5 +1,5 @@
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCW37ewW5LQuc5wT3HUFarLuut0B7iouk&callback=initMap" defer ></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKmzL_GF_sQJjHj4d_gva-1ptGdWMPFkQ&callback=initMap&libraries=places" defer ></script>
     <style type="text/css">
         #map {
               width: 100%;
@@ -28,7 +28,64 @@
                 },*/
             });
 
-            geocoder = new google.maps.Geocoder();
+            const input = document.getElementById("pac-input");
+            const searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener("bounds_changed", () => {
+              searchBox.setBounds(map.getBounds());
+            });
+            let markers = [];
+
+
+            // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener("places_changed", () => {
+          const places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+          // Clear out the old markers.
+          markers.forEach((marker) => {
+            marker.setMap(null);
+          });
+          markers = [];
+          // For each place, get the icon, name and location.
+          const bounds = new google.maps.LatLngBounds();
+          places.forEach((place) => {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            const icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25),
+            };
+            // Create a marker for each place.
+            markers.push(
+              new google.maps.Marker({
+                map,
+                icon,
+                title: place.name,
+                position: place.geometry.location,
+              })
+            );
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+
+            /*geocoder = new google.maps.Geocoder();
 
             // Configure the click listener.
             map.addListener('click', function(mapsMouseEvent) {
@@ -55,7 +112,7 @@
 
                 //Get the marker's location.
                 markerLocation();
-            });
+            });*/
         }
 
         //This function will get the marker's current location and then add the lat/long
@@ -97,7 +154,9 @@
                 <h2 class="text-white">Selct Your Location</h2>
                 <p class="m-0">Click on a location on the map to select it. Drag the marker to change location.</p>
             </div>
+
             <div class="card-body p-0 h-100vh">
+                <input id="pac-input" class="controls" type="text" placeholder="Search Box"/>
                 <!-- <h1>Select a location!</h1> -->
                 <!--map div-->
                 <div id="map"></div>
