6a887588363323df451624714108f8fd2049663a
sajeer <sajeer.ali09@gmail.com>
2020-11-11 21:24:33 +0400

    Update EMSB.php
    done

--- application/views/reportspdf/EMSB.php
+++ application/views/reportspdf/EMSB.php
@@ -7,101 +7,112 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        body {
-            margin: 0;
-            margin-bottom: 1cm;
-            margin-top: 1cm;
-            padding: 0;
-            background-color: #FAFAFA;
-            font: 12pt "Tahoma";
-        }
+    body {
+        margin: 0;
+        margin-bottom: 1cm;
+        margin-top: 1cm;
+        padding: 0;
+        background-color: #FAFAFA;
+        font: 12pt "Tahoma";
+    }
 
+    .divFooter {
+        position: fixed;
+        bottom: 0cm;
+        left: 0.5cm;
+        right: 0cm;
+        height: 1cm;
+    }
+
+    .footer {
+        position: fixed;
+        bottom: 0px;
+    }
+
+    .pagenum:before {
+        content: counter(page);
+    }
+
+    * {
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+    }
+
+    .page {
+        width: 20cm;
+        min-height: 29.7cm;
+        padding: 0.5cm;
+        margin: 0px;
+        border-radius: 5px;
+        background: white;
+    }
+
+
+    @page {
+        size: A4;
+        margin: 0cm;
+    }
+
+    @media print {
+        .page {
+            margin: 0;
+            border: initial;
+            border-radius: initial;
+            width: initial;
+            min-height: initial;
+            box-shadow: initial;
+            background: initial;
+            page-break-after: always;
+        }
+    }
+
+    #customers {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    #customers td,
+    #customers th {
+        padding: 8px;
+        font-size: 12px;
+    }
+
+    #customers tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    #customers tr:hover {
+        background-color: #ddd;
+    }
+
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #8443ff;
+        color: white;
+    }
+
+    @media print {
         .divFooter {
             position: fixed;
-            bottom: 0cm; 
-            left: 0.5cm; 
-            right: 0cm;
-            height: 1cm;
+            bottom: 0;
         }
-
-        .footer { position: fixed; bottom: 0px; }
-        .pagenum:before { content: counter(page); }
-
-        * {
-            box-sizing: border-box;
-            -moz-box-sizing: border-box;
-        }
-
-        .page {
-            width: 20cm;
-            min-height: 29.7cm;
-            padding: 0.5cm;
-            margin: 0px;
-            border-radius: 5px;
-            background: white;
-        }
-
-
-        @page {
-            size: A4;
-            margin: 0cm;
-        }
-
-        @media print {
-            .page {
-                margin: 0;
-                border: initial;
-                border-radius: initial;
-                width: initial;
-                min-height: initial;
-                box-shadow: initial;
-                background: initial;
-                page-break-after: always;
-            }
-        }
-
-        #customers {
-            font-family: Arial, Helvetica, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        #customers td,
-        #customers th {
-            padding: 8px;
-            font-size: 12px;
-        }
-
-        #customers tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        #customers tr:hover {
-            background-color: #ddd;
-        }
-
-        #customers th {
-            padding-top: 12px;
-            padding-bottom: 12px;
-            text-align: left;
-            background-color: #8443ff;
-            color: white;
-        }
-
-        @media print {
-            .divFooter {
-                position: fixed;
-                bottom: 0;
-            }
-        }
+    }
     </style>
 </head>
 
 <body>
     <!-- footer -->
-    <div class="divFooter footer" style="font-size:12px; padding: 20px 0px; width: 100%">
+    <div class="divFooter footer" style="font-size:12px; width: 100%">
         <hr>
-        Printed on: <?php echo date("l, d F, Y H:i:s A"); ?><br/>Page: <span class="pagenum"></span>
+        <table style="width:100%">
+            <tr>
+                <td style="width:50%" align="left">Printed on: <?php echo date("l, d F, Y H:i:s A"); ?></td>
+                <td style="text-align-right width:50%;" align="right">Page: <span class="pagenum"></span>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            </tr>
+        </table>
     </div>
     <!-- end footer -->
 
@@ -136,8 +147,8 @@
             </table>
             <!-- end header -->
             <hr>
-             <!-- header -->
-             <table style="width:100%">
+            <!-- header -->
+            <table style="width:100%">
                 <tr>
                     <td style="width:100%">
                         <table style="font-size:12px; margin-top: 20px; margin-bottom:20px;">
@@ -157,7 +168,7 @@
 
 
 
-        
+
 
 
             <table id="customers" style="width:100%">
@@ -234,7 +245,8 @@
                     <td style="border-top: 2px solid #333; border-bottom: 2px solid #333;"><strong>0.00</strong></td>
                     <td style="border-top: 2px solid #333; border-bottom: 2px solid #333;"><strong>5,056.22</strong>
                     </td>
-                    <td style="border-top: 2px solid #333; border-bottom: 2px solid #333; text-align: right"><strong>5,056.22</strong>
+                    <td style="border-top: 2px solid #333; border-bottom: 2px solid #333; text-align: right">
+                        <strong>5,056.22</strong>
                     </td>
 
                 </tr>
